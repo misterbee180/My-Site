15ff5192ec5374ad591a8a5a51086b2c2a87a6fe
misterbee180 <misterbee180@gmail.com>
2015-03-01 21:01:24 -0600

    Most recent changes

--- FTPMyFile.bat
+++ FTPMyFile.bat
@@ -1,3 +1,7 @@
+:: Confirm that a file was passed in and that it exists
+if "%1" == "" goto NoArgument
+if not exist %1 goto FileNotExist
+
 echo a2819517>>ftp_temp.txt
 echo flames02>>ftp_temp.txt
 echo cd public_html>>ftp_temp.txt
@@ -5,3 +9,10 @@ echo put %1>>ftp_temp.txt
 echo quit>>ftp_temp.txt
 ftp -s:ftp_temp.txt misterbee180.herobo.com
 del ftp_temp.txt
+goto:eof
+
+:: Error handling
+:NoArgument
+Echo "No file Passed In"
+:FileNotExist
+Echo "File does not exist"
